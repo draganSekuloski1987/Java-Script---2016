ac46ad28e03a32450c47d973707ab8814bd97877
Dragan Sekuloski <sekuloski.dragan@gmail.com>
2017-01-31 17:16:41 +0100

    Add Elements to HTML
    -Via JQuery (commented)
    -Via JavaScript uncommented

---
+++ Add Element/script.js
@@ -0,0 +1,57 @@
+
+// $(function(){});// JQuery on ready long form
+
+// $(() => {}); // JQuery on ready short form
+
+// $(() => {
+//     let result = $('#result');
+
+//     $('#add').on('click', () => {
+//         let tds = $('td');
+//         console.log(tds);
+
+//         for (let index = 0; index <tds.length; index++){
+//             let td = $(tds[index]);
+//             // td.append( "<button>Button "+ index + "</button>" )
+//             let button = document.createElement("button");
+//             button.innerHTML = `Button #${index+1}`;
+//             button.addEventListener('click', ()=>{
+//                 result.text(`Hi, I'am button #${index+1}`)
+//             })
+
+
+//             td.append(button);
+
+
+//         }
+//         $('#add').remove();
+//     })
+
+// });
+
+document.addEventListener('DOMContentLoaded', (event) => {
+
+    let result = document.getElementById('result');
+
+    document.getElementById('add').addEventListener('click', (event) => {
+        let tds = document.getElementsByTagName('td');
+        console.log(tds);
+
+        for (let index = 0; index < tds.length; index++) {
+            let td = tds[index];
+            console.log(td);
+
+            let button = document.createElement('button');
+            button.innerHTML = `Button #${index + 1}`;
+            button.addEventListener('click', (event) => {
+                result.innerHTML = `Hello I am button #${index + 1}`;
+            })
+
+            td.appendChild(button);
+
+        }
+
+
+    })
+
+});
